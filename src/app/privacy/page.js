8eec4fc72861e8bb868bbef87398f473aa35a7c6
Fardin7864 <fardinhasan1888@gmail.com>
2024-05-27 11:00:29 +0600

    add privacy

---
+++ src/app/privacy/page.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Our Privacy Policy</h1>
+      <p>
+        1. Introduction Welcome to [Your E-commerce Website Name]. We are
+        committed to protecting the privacy of our visitors and customers. This
+        Privacy Policy explains how we collect, use, disclose, and safeguard
+        your information when you visit our website [website address], including
+        any other media form, media channel, mobile website, or mobile
+        application related or connected thereto. Please read this privacy
+        policy carefully. If you do not agree with the terms of this privacy
+        policy, please do not access the site. 2. Information We Collect We
+        collect information that you provide to us directly and information
+        automatically collected when you use our services. The information we
+        may collect includes but is not limited to: Personal Data: Personally
+        identifiable information, such as your name, shipping address, email
+        address, and telephone number, that you voluntarily give to us when you
+        register with the website or when you choose to participate in various
+        activities related to the website, such as online chat and message
+        boards. Derivative Data: Information our servers automatically collect
+        when you access the website, such as your IP address, your browser type,
+        your operating system, your access times, and the pages you have viewed
+        directly before and after accessing the site. Financial Data: Financial
+        information, such as data related to your payment method (e.g. valid
+        credit card number, card brand, expiration date) that we may collect
+        when you purchase, order, return, exchange, or request information about
+        our services from the site. 3. Use of Your Information Having accurate
+        information about you permits us to provide you with a smooth,
+        efficient, and customized experience. Specifically, we may use
+        information collected about you via the site to: Process purchases and
+        returns. Increase the efficiency and operation of the site. Monitor and
+        analyze usage and trends to improve your experience with the site.
+        Perform other business activities as needed. 4. Disclosure of Your
+        Information We may share information we have collected about you in
+        certain situations. Your information may be disclosed as follows: By Law
+        or to Protect Rights: If we believe the release of information about you
+        is necessary to respond to legal process, to investigate or remedy
+        potential violations of our policies, or to protect the rights,
+        property, and safety of others, we may share your information as
+        permitted or required by any applicable law, rule, or regulation.
+        Third-Party Service Providers: We may share your information with third
+        parties that perform services for us or on our behalf, including payment
+        processing, data analysis, email delivery, hosting services, customer
+        service, and marketing assistance. 5. Security of Your Information We
+        use administrative, technical, and physical security measures to help
+        protect your personal information. While we have taken reasonable steps
+        to secure the personal information you provide to us, please be aware
+        that despite our efforts, no security measures are perfect or
+        impenetrable, and no method of data transmission can be guaranteed
+        against any interception or other type of misuse.
+      </p>
+    </div>
+  );
+};
+
+export default page;
